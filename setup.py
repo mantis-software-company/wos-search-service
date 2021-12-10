4d6d79340609225098a5ad470227d64ee70f67b7
Furkan Kalkan <furkankalkan@mantis.com.tr>
2021-12-10 22:09:44 +0300

    setup.py fix

--- setup.py
+++ setup.py
@@ -26,5 +26,6 @@ setuptools.setup(
     install_requires=['Flask~=2.0.2', 'flask-smorest~=0.35.0', 'marshmallow~=3.14.0', 'pyctuator~=0.16.0',
                       'wos~=0.2.5', 'lxml~=4.6.4'],
     python_requires=">3.8.*, <4",
-    packages=setuptools.find_packages()
+    packages=setuptools.find_packages(where="src"),
+    package_dir={"": "src"}
 )
